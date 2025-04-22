ee206490fd6cdce60e4fc492a957b77549d83925
Rafaelsn11 <faelsantana001@gmail.com>
2025-04-22 16:34:29 -0300

    fix: correct password validation to use Equal instead of Matches

--- RO.DevTest.Application/Features/User/Commands/CreateUserCommand/CreateUserCommandHandler.cs
+++ RO.DevTest.Application/Features/User/Commands/CreateUserCommand/CreateUserCommandHandler.cs
@@ -9,28 +9,33 @@ namespace RO.DevTest.Application.Features.User.Commands.CreateUserCommand;
 /// <summary>
 /// Command handler for the creation of <see cref="Domain.Entities.User"/>
 /// </summary>
-public class CreateUserCommandHandler(IIdentityAbstractor identityAbstractor) : IRequestHandler<CreateUserCommand, CreateUserResult> {
+public class CreateUserCommandHandler(IIdentityAbstractor identityAbstractor) : IRequestHandler<CreateUserCommand, CreateUserResult> 
+{
     private readonly IIdentityAbstractor _identityAbstractor = identityAbstractor;
 
-    public async Task<CreateUserResult> Handle(CreateUserCommand request, CancellationToken cancellationToken) {
-        CreateUserCommandValidator validator = new();
-        ValidationResult validationResult = await validator.ValidateAsync(request, cancellationToken);
-
-        if(!validationResult.IsValid) {
-            throw new BadRequestException(validationResult);
-        }
+    public async Task<CreateUserResult> Handle(CreateUserCommand request, CancellationToken cancellationToken) 
+    {
+        await Validate(request, cancellationToken);
 
         Domain.Entities.User newUser = request.AssignTo();
         IdentityResult userCreationResult = await _identityAbstractor.CreateUserAsync(newUser, request.Password);
-        if(!userCreationResult.Succeeded) {
+        if(!userCreationResult.Succeeded)
             throw new BadRequestException(userCreationResult);
-        }
 
         IdentityResult userRoleResult = await _identityAbstractor.AddToRoleAsync(newUser, request.Role);
-        if(!userRoleResult.Succeeded) {
+        if(!userRoleResult.Succeeded)
             throw new BadRequestException(userRoleResult);
-        }
 
         return new CreateUserResult(newUser);
+    }
+
+    private async Task Validate(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        CreateUserCommandValidator validator = new();
+        ValidationResult validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+        if(!validationResult.IsValid)
+            throw new BadRequestException(validationResult);
+       
     }
 }
